b3cbb4214e8056e3728f1f1cd24c40c351d641b9
Costin Iordache <iordachecostin4@gmail.com>
2026-03-12 08:38:43 +0200

    asertii spi

--- ve/spi_interface.sv
+++ ve/spi_interface.sv
@@ -33,15 +33,29 @@ interface spi_intf(input logic clk,reset);
   modport MONITOR (clocking monitor_cb,input clk,reset);
 
   property p_sclk_active_only_when_ss_active;
-    @(posedge clk) disable iff (reset) (ss == 1'b0) |-> (!isunknown(sclk));
+    @(posedge clk) disable iff (reset==0) (ss == 1'b0) |-> (!isunknown(sclk));
   endproperty
 
   property p_mosi_valid_only_when_ss_active;
-    @(posedge clk) disable iff (reset) (ss == 1'b0) |-> (!isunknown(mosi));
+    @(posedge clk) disable iff (reset==0) (ss == 1'b0) |-> (!isunknown(mosi));
   endproperty
 
   property p_miso_valid_only_when_ss_active;
-    @(posedge clk) disable iff (reset) (ss == 1'b0) |-> (!isunknown(miso));
+    @(posedge clk) disable iff (reset==0) (ss == 1'b0) |-> (!isunknown(miso));
   endproperty
-  
+
+  miso_valid: assert property (p_miso_valid_only_when_ss_active) 
+  else $error("MISO is valid while SS is inactive");
+  MISO_VALID: cover property (p_miso_valid_only_when_ss_active);
+
+  mosi_valid: assert property (p_mosi_valid_only_when_ss_active) 
+  else $error("MOSI is valid while SS is inactive");
+  MOSI_VALID: cover property (p_mosi_valid_only_when_ss_active);
+
+  sclk_valid: assert property (p_sclk_active_only_when_ss_active) 
+  else $error("SCLK is active while SS is inactive");
+  SCLK_VALID: cover property (p_sclk_active_only_when_ss_active);
+
+
+
 endinterface
