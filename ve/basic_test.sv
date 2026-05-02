80496b134aefff6ff53fb734275768b43d67cea3
Costin Iordache <iordachecostin4@gmail.com>
2026-05-02 09:22:52 +0300

    revised random_test

--- ve/basic_test.sv
+++ ve/basic_test.sv
@@ -22,8 +22,8 @@ program test(vr_intf vr_intf, spi_intf spi_intf, reset_intf rst_intf);
     env = new(vr_intf, spi_intf, rst_intf);
     
     //setting the repeat count of generator as 4, means to generate 4 packets
-    env.vr_gen.repeat_count = 20;
-    env.spi_gen.repeat_count = 20;
+    env.vr_gen.repeat_count = 4;
+    env.spi_gen.repeat_count = 4;
 
     env.vr_gen.trans = good_trans;
     
--- ve/random_test.sv
+++ ve/random_test.sv
@@ -4,18 +4,19 @@
 
 //tranzactiile din acest text se genereaza complet aleatoriu (singura constrangere fiind in fisierul transaction.sv, aceasta asigurand functionalitatea corecta a DUT-ului)
 `include "environment.sv"
-program test(mem_intf intf);
+program test(vr_intf vr_intf, spi_intf spi_intf, reset_intf rst_intf);
   
   //declaring environment instance
   environment env;
   
   initial begin
     //creating environment
-    env = new(intf);
+    env = new(vr_intf, spi_intf, rst_intf);
     
     //setting the repeat count of generator as 4, means to generate 4 packets
-    env.gen.repeat_count = 4;
-    
+    env.vr_gen.repeat_count = 20;
+    env.spi_gen.repeat_count = 20;
+
     //calling run of env, it interns calls generator and driver main tasks.
     env.run();
   end
